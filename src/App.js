1782017467c92120044287eb6054ef5e34eb4887
Allan Tabri <allan.tabri@mail.ee>
2022-06-19 19:34:42 +0300

    changed UI and added feedback logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import { useState } from "react";
+import {v4 as uuidv4} from 'uuid'
 import Header from "./Components/Header";
 import FeedbackList from "./Components/FeedbackList";
 import FeedbackData from './data/FeedbackData';
-
+import FeedbackStats from "./Components/FeedbackStats";
+import FeedbackForm from "./Components/FeedbackForm";
 
 function App() {
 
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) =>{
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback]) 
+  }
+
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure?')){
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
 
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback}/>
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback}
+        handleDelete={deleteFeedback}/>
       </div>
     </>
   );
--- src/Components/Feedbackitem.jsx
+++ src/Components/Feedbackitem.jsx
@@ -4,11 +4,14 @@ import PropTypes from 'prop-types'
 
 
 
-function FeedbackItem( {item} ) {
+function FeedbackItem( {item, handleDelete} ) {
   
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={() => handleDelete(item.id)}>
+        <FaTimes color='purple' />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
